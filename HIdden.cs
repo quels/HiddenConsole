3899e951c0252c1497a6700a96adf9ca7e6a1cb2
quels <35525737+quels@users.noreply.github.com>
2019-12-22 12:44:27 +0200

    Hidden.cs
    use the class by typing:
    HideWindow win = new HideWindow(true);// this hides your console window
    to use the class again you can use 
    win.ChangeHidden(false); // this shows your console window

---
+++ HIdden.cs
@@ -0,0 +1,43 @@
+class HideWindow
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+        public HideWindow(bool type)
+        {
+            IntPtr hWnd = GetConsoleWindow();
+            if (type)
+            {
+                if (hWnd != IntPtr.Zero)
+                {
+                    ShowWindow(hWnd, 0);
+
+                }
+
+            }
+            else
+                if (hWnd != IntPtr.Zero)
+                {
+                    ShowWindow(hWnd, 1);
+                }
+        }
+        public void ChangeHidden(bool type)
+        {
+            IntPtr hWnd = GetConsoleWindow();
+            if (type)
+            {
+                if (hWnd != IntPtr.Zero)
+                {
+                    ShowWindow(hWnd, 0);
+
+                }
+
+            }
+            else
+                if (hWnd != IntPtr.Zero)
+            {
+                ShowWindow(hWnd, 1);
+            }
+        }
+    }
